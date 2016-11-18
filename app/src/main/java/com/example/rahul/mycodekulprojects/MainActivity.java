b01380343d0a8aedfa5d57370193685530fc9e7f
Rahul <rswagh14@yahoo.com>
2016-11-18 17:31:34 +0530

    17/11/2016 - Button

--- app/src/main/java/com/example/rahul/mycodekulprojects/MainActivity.java
+++ app/src/main/java/com/example/rahul/mycodekulprojects/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.rahul.mycodekulprojects;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btngood,btnbetter,btnbest;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.text);
+
+        btngood = (Button)findViewById(R.id.btngood);
+        btngood.setOnClickListener(this::goodClicked);
+
+        btnbetter = (Button)findViewById(R.id.btnbetter);
+        btnbetter.setOnClickListener(this::betterClicked);
+
+        btnbest = (Button)findViewById(R.id.btnbest);
+        btnbest.setOnClickListener(this::bestClicked);
+
     }
+
+    private void goodClicked(View view){
+        textView.setText("Good Clicked");
+    }
+
+    private void betterClicked(View view){
+        textView.setText("Better Clicked");
+    }
+
+    private void bestClicked(View view){
+        textView.setText("Best Clicked");
+    }
+
 }
