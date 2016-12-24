180e327b29e1e0fd54e8360a17c8f087af2c99a1
Rahul <rswagh14@yahoo.com>
2016-12-24 13:36:07 +0530

    Content Provider, Whats app like Fragments

---
+++ whatsappfragments/src/main/java/com/example/whatsappfragments/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.whatsappfragments;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+          tabLayout.getTabAt(0).setText("CALLS").setIcon(R.drawable.ic_local_phone_black_24dp);
+          tabLayout.getTabAt(1).setText("CHATS").setIcon(R.drawable.ic_local_phone_black_24dp);
+          tabLayout.getTabAt(2).setText("CONTACTS").setIcon(R.drawable.ic_local_phone_black_24dp);
+
+
+    }
+
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            Fragment fragment = null;
+            if(position == 0) fragment = new CALLS();
+            else if(position == 1) fragment = new CHATS();
+            else fragment = new CONTACTS();
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "CALLS";
+                case 1:
+                    return "CHATS";
+                case 2:
+                    return "CONTACTS";
+            }
+            return null;
+        }
+    }
+}
